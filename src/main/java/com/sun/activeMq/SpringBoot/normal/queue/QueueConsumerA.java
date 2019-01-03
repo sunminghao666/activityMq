eda236fc7f41babea54ecf7b5a98f1dd84df9a94
sunminghao <sunminghao@youxin.com>
2019-01-03 17:19:35 +0800

    Native

--- src/main/java/com/sun/activeMq/SpringBoot/normal/queue/QueueConsumerA.java
+++ src/main/java/com/sun/activeMq/SpringBoot/normal/queue/QueueConsumerA.java
@@ -8,7 +8,7 @@ public class QueueConsumerA {
 
     // 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
     @JmsListener(destination = "springboot.queue")
-  //@JmsListener(destination = "springboot.queue") 同一个方法可以侦听多个队列
+    //@JmsListener(destination = "springboot.queue") 同一个方法可以侦听多个队列
     public void receiveQueue(String text){
         System.out.println(this.getClass().getName()+" receive msg:"+text);
     }
